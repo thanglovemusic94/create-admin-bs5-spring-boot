d55fece902a099f5d6ef68c23a118b19212842a0
thangtony <thanglovemusic94@gmail.com>
2021-11-14 21:40:27 +0700

    init project

---
+++ src/main/java/com/example/createadminbs5springboot/CreateAdminBs5SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.createadminbs5springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateAdminBs5SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateAdminBs5SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/createadminbs5springboot/CreateAdminBs5SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createadminbs5springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateAdminBs5SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
